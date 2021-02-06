f130a3b296d076dee01395c1355e16989f543259
Isa1Maria <50515191+Isa1Maria@users.noreply.github.com>
2021-02-06 16:17:57 +0000

    Navbar button

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,27 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import './Navbar.css';
+import {Button} from './Button';
 
 
 function Navbar() {
 
     const[click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
-
     const closeMbileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false);
+        }
+        else {
+            setButton(true);
+        }
+    };
+
+    window.addEventListener("resize", showButton);
 
     return (
         <>
@@ -42,11 +55,11 @@ function Navbar() {
         </Link>
     </li>
     </ul>
-
+ {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
 </div>
             </nav>
         </>
     )
 }
 
-export default Navbar
+export default Navbar;
