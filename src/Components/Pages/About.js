6a0a8cdb4112a532f4d723163ce8cd249cebc326
Isa1Maria <50515191+Isa1Maria@users.noreply.github.com>
2021-02-09 22:22:55 +0000

    About

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -1,18 +1,14 @@
 import React from 'react';
+import '../Main.css';
 
 function About() {
     return (
-        <div>
-              <p>
-          Wouldn't it be great to have your own garden?
-          <br/>
-          But you don't know how to start?
-          <br/>
-          With Hortíos you can learn how to!
-        </p>
-        <button type="button" class="btn btn-success">
-          Start cultivating!
-        </button>
+      <div className="main-container">
+        <h2>
+           Hortíos is a space where you can learn how to cultivate various vegetables, fruits and aromatic herbs.
+           <br/>
+           We give you information on how and when to cultivate in the peace of your home.
+        </h2>
         </div>
     )
 }
