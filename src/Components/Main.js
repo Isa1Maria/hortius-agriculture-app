7dc4c69ee033a9e2d832d2a3dde7b027893e97b3
Isa1Maria <50515191+Isa1Maria@users.noreply.github.com>
2021-02-11 16:38:12 +0000

    BackgroundImage responsive mobile

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React ,{useState, useEffect} from 'react';
 import '../App.css';
 import './Main.css';
 import {Button} from './Button';
 import backgroundImage from '../Components/Images/backgroundImage.jpg';
+import backgroundImage2 from '../Components/Images/backgroundImage2.jpg';
 
 
 
-function Main() {
+const Main = () => {
+ 
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const imageUrl = windowWidth >= 650 ? backgroundImage : backgroundImage2;
+
+    useEffect(() => {
+        const handleWindowResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+        
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        }
+    }, []);
+
     return (
         <div className="main-container" 
-        style={{ backgroundImage: `url(${backgroundImage})` }}
+        style={{ backgroundImage: `url(${imageUrl})` }}
         >
          
             <h1> Wouldn't it be great to have your own garden?</h1>
